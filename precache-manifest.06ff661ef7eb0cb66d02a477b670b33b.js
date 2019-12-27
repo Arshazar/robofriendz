affd9de62cd8d6495985c870b481daefb36a2729
arshavir azarfar <altair.cena@gmail.com>
2019-12-27 20:09:10 +0330

    Updates

---
+++ precache-manifest.06ff661ef7eb0cb66d02a477b670b33b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b450edd79a7520011c333c4068c24e",
+    "url": "/robofriendz/index.html"
+  },
+  {
+    "revision": "e24e42d11b6602f8911d",
+    "url": "/robofriendz/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "46e3e213a17c942becc7",
+    "url": "/robofriendz/static/css/main.a27c6058.chunk.css"
+  },
+  {
+    "revision": "e24e42d11b6602f8911d",
+    "url": "/robofriendz/static/js/2.4cd5622b.chunk.js"
+  },
+  {
+    "revision": "46e3e213a17c942becc7",
+    "url": "/robofriendz/static/js/main.92df6705.chunk.js"
+  },
+  {
+    "revision": "543a48ce686bd0badc9f",
+    "url": "/robofriendz/static/js/runtime-main.0bd370a6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendz/static/media/SEGA.12a3db8b.woff"
+  }
+]);
